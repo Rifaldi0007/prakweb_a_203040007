ec4e32998573ec7bdf03b91dfebf4d70fb128003
Rifaldi0007 <rifaldia513@gmail.com>
2022-10-03 22:06:04 +0700

    menyelesaikan CRUD

---
+++ pw/db.php
@@ -0,0 +1,8 @@
+<?php 
+    $hostname = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname =  'prakweb_a_203040007_db';
+
+    $conn = mysqli_connect($hostname, $username, $password, $dbname) or die ('Failed connect to database');
+ ?>
