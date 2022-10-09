a9b72f1ecae4844e0ec610b5aa02f072f1e96d6a
Rifaldi0007 <rifaldia513@gmail.com>
2022-10-09 17:51:01 +0700

    menyelesaikan video asset

--- mvc/app/init.php
+++ mvc/app/init.php
@@ -2,3 +2,5 @@
 
 require_once 'core/app.php';
 require_once 'core/controller.php';
+
+require_once 'core/constants.php';
--- mvc/app/views/home/index.php
+++ mvc/app/views/home/index.php
@@ -1,2 +1,3 @@
-
+<div class="container">
     <h1>selamat datang di web kami</h1>
+</div>
