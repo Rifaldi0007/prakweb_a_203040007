c2b2273cd1160aae82edc9afa58c65482614f3db
Rifaldi0007 <rifaldia513@gmail.com>
2022-10-10 21:33:19 +0700

    update video 7

--- mvc/models/Mahasiswa_model.php
+++ mvc/models/Mahasiswa_model.php
@@ -1,17 +1,25 @@
 <?php
 
 class Mahasiswa_model {
-    private $mhs = [
-        [
-            "nama" => "Faldi Arsan",
-            "nrp" => "203040007",
-            "email" => "faldi@gmail.com",
-            "jurusan" => "Teknik Informatika"
-        ]
-    ];
+    private $dbh; //database handler
+    private $stmt;
+
+    public function __construct()
+    {
+        //data source name
+        $dsn = 'mysql:host=localhost;dbname=mvc';
+
+        try {
+            $this->dbh = new PDO($dsn, 'root', '');
+        }catch(PDOException $e){
+            die($e->getMessage());
+        }
+    }
 
     public function getAllMahasiswa()
     {
-        return $this->mhs;
+        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+        $this->stmt->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
