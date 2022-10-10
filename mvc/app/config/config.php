d98107a70f3f702c97c9900393d08f8818ccf0e6
Rifaldi0007 <rifaldia513@gmail.com>
2022-10-10 22:32:34 +0700

    Menyelesaikan video 8

---
+++ mvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_a_203040007/public');
+
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvc');
--- mvc/app/core/constants.php
+++ mvc/app/core/constants.php
@@ -1,3 +1,2 @@
 <?php
 
-define('BASEURL', 'http://localhost/prakweb_a_203040007/public');
